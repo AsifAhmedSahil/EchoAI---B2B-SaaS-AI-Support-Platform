f458bca4f7449f1e57457558cc6293f30aee0daa
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-08-23 12:31:06 +0600

    feat: implement clerk with middleware and login functionality

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { useMutation, useQuery } from "convex/react"
+import { useMutation, useQuery,Authenticated, Unauthenticated } from "convex/react"
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 import {api} from "@workspace/backend/_generated/api"
 
@@ -7,11 +8,21 @@ export default function Page() {
   const users = useQuery(api.users.getMany)
   const addUser = useMutation(api.users.add)
   return (
+    <>
+    <Authenticated>
+
     <div className="flex items-center justify-center min-h-svh">
       <p>apps/web</p>
+      <UserButton/>
       {
         JSON.stringify(users)
       }
     </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>must be sign in...</p>
+        <SignInButton/>
+      </Unauthenticated>
+      </>
   )
 }
