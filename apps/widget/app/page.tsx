9442f8f03b1a3f3b22419c3950326b06dba136f0
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-08-23 10:30:58 +0600

    2nd app setup widget

---
+++ apps/widget/app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useQuery } from "convex/react"
+
+import {api} from "@workspace/backend/_generated/api"
+
+export default function Page() {
+  const users = useQuery(api.users.getMany)
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <p>apps/widget</p>
+      {
+        JSON.stringify(users)
+      }
+    </div>
+  )
+}
